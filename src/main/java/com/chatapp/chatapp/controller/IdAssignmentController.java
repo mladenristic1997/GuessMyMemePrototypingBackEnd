6003ff3b7abe34d42164f04356beb16446459943
djomlaks <mladenristic1997@gmail.com>
2018-09-13 09:17:23 +0200

    fixing getRoomId using @MessageMapping

--- src/main/java/com/chatapp/chatapp/controller/IdAssignmentController.java
+++ src/main/java/com/chatapp/chatapp/controller/IdAssignmentController.java
@@ -1,16 +1,21 @@
 package com.chatapp.chatapp.controller;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import com.chatapp.chatapp.model.Game;
 import com.chatapp.chatapp.model.User;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import com.google.gson.Gson;
 
 
 import java.util.ArrayList;
+import java.util.Map;
 
 @RestController
 public class IdAssignmentController {
@@ -23,17 +28,16 @@ public class IdAssignmentController {
     private SimpMessageSendingOperations messagingTemplate;
 
 
-    @GetMapping
-    @RequestMapping("/getRoomId")
-    @ResponseStatus(HttpStatus.OK)
+    @MessageMapping("/getRoomId")
     @CrossOrigin(origins = "http://localhost:4200")
-    public void uniqueRoomId(@RequestParam("username") String username) throws InterruptedException {
-        //if someone is in the lobby then generate a game with the first one
+    public void uniqueRoomId(@Payload String player) throws InterruptedException {
+        //if someone is in the lobby then generate a game with the first
+        String username = new Gson().fromJson(player, Map.class).get("username").toString();
+
         for(User user : lobby){
             //generate room here
             //now we hardcode 100 as the number of memes, but when we implement a database we will insert that number dynamically
             Game game = new Game(user.getName(), username, 100);
-            game.setRoomId("" + System.currentTimeMillis());
             String gameJson = new Gson().toJson(game);
             //send to player who was in the lobby
             String url1 = "/topic/reply/" + user.getName();
@@ -41,8 +45,8 @@ public class IdAssignmentController {
             String url2 = "/topic/reply/" + username;
             messagingTemplate.convertAndSend(url1, gameJson);
             messagingTemplate.convertAndSend(url2, gameJson);
-            lobby.remove(user);
             setUsersToIngame(user.getName(), username);
+            lobby.remove(user);
             return;
         }
         //If no one is in the lobby, add the player to lobby
@@ -51,12 +55,13 @@ public class IdAssignmentController {
                 if(lobby.contains(user)) return;
 
                 lobby.add(user);
+                return;
             }
         }
     }
 
     @GetMapping
-    @RequestMapping("sendInvitation")
+    @RequestMapping("/sendInvitation")
     @ResponseStatus(HttpStatus.OK)
     @CrossOrigin(origins = "http://localhost:4200")
     public void sendInvitation(@RequestParam("requestingPlayer") String requestingPlayer, @RequestParam("otherPlayer") String otherPlayer){
@@ -68,7 +73,7 @@ public class IdAssignmentController {
     }
 
     @GetMapping
-    @RequestMapping("acceptInvitation")
+    @RequestMapping("/acceptInvitation")
     @ResponseStatus(HttpStatus.OK)
     @CrossOrigin(origins = "http://localhost:4200")
     public void acceptInvitation(@RequestParam("playerOne") String playerOne, @RequestParam("playerTwo") String playerTwo){
